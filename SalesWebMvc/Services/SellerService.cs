9532ce2f1d1b79ae986ffb43f068de013ce5c832
Leandro Henrique <leandro2600@gmail.com>
2022-06-20 17:52:43 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -25,5 +25,17 @@ namespace SalesWebMvc.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
